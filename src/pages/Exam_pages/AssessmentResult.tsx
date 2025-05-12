85dc165485f13c4479305b1b7f540998d8814baa
Riyan Jamil <riyanjamil220@gmail.com>
2025-05-12 22:43:49 +0500

    Designing rough assesment result page

--- src/pages/Exam_pages/AssessmentResult.tsx
+++ src/pages/Exam_pages/AssessmentResult.tsx
@@ -1,8 +1,192 @@
+import { Box, Paper, SelectChangeEvent, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material"
+import { useEffect, useState } from "react";
 
+const sessions = [
+  {
+    value: 'Spring 2025',
+    label: 'Spring 2025',
+  },
+  {
+    value: 'Fall 2024',
+    label: 'Spring 2024',
+  },
+  {
+    value: 'Fall 2023',
+    label: 'Fall 2023',
+  },
+];
 
+const courses = [
+  {
+    value: 'Introduction To Database Systems',
+    label: 'Introduction To Database Systems'
+  },
+  {
+    value: 'Introduction To Database Systems Lab',
+    label: 'Introduction To Database Systems Lab'
+  },
+  {
+    value: 'Probability & Statistics',
+    label: 'Probability & Statistics'
+  },
+  {
+    value: 'Technical Report Writing',
+    label: 'Technical Report Writing'
+  },
+  {
+    value: 'Software Design & Architecture',
+    label: 'Software Design & Architecture'
+  },
+  {
+    value: 'Software Design & Architecture Lab',
+    label: 'Software Design & Architecture Lab'
+  }
+
+]
+
+const createData = (
+  assementType: string,
+  assessment: string,
+  courseCode: string,
+  sectionCode: string,
+  teacher: string,
+  totalMarks: number,
+  marksObtained: number
+) => {
+  return {
+    assementType, assessment, courseCode, sectionCode, teacher, totalMarks, marksObtained
+  }
+}
+
+const rows = [
+  createData('Mid Term', 'MT1', 'INTRODUCTION TO DATABASE SYSTEMS, ( SE209T )', 'Spring 2025-2023F-BS-SE-SE209T-B-17231',
+    'Engr. Kiran Hidayat', 15, 14),
+  createData('Mid Term', 'MT2', 'INTRODUCTION TO DATABASE SYSTEMS, ( SE209T )', 'Spring 2025-2023F-BS-SE-SE209T-B-17231',
+    'Engr. Kiran Hidayat', 15, 14),
+]
 
 export const AssessmentResult = () => {
+  const [selectedCourse, setSelectedCourse] = useState("");
+  const [filteredRows, setFilteredRows] = useState(rows);
+
+
+  const handleCourseChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedCourse(event.target.value);
+  };
+
+  useEffect(() => {
+    // Filter the rows based on the selected course whenever the course changes
+    if (selectedCourse) {
+      const newFilteredRows = rows.filter(row =>
+        row.courseCode.toLowerCase().includes(selectedCourse.toLowerCase())
+      );
+      setFilteredRows(newFilteredRows);
+    } else {
+      setFilteredRows(rows);
+    }
+  }, [selectedCourse]);
+
   return (
-    <div>AssessmentResult</div>
+    <Box sx={{ display: 'flex', flexDirection: 'column', overflow: "auto" }} >
+      <Paper sx={{ padding: 2, overflow: 'auto', maxHeight: '680px' }}>
+        <Box
+          sx={{
+            position: "sticky",
+            top: '-17px',
+            zIndex: 1,
+            backgroundColor: "white",
+            textAlign: "center",
+            py: 2,
+            minWidth: '1100px',
+          }}
+        >
+          <div className="text-xl font-semibold text-gray-800 sm:text-2xl sm:mb-4 md:text-3xl">Assesment Result</div>
+          <Box sx={{ display: 'flex', flexDirection: 'row', gap: '1rem', marginTop: 4, minWidth: '1100px', width: '100%' }}>
+            <Box
+              component="form"
+              sx={{ '& .MuiTextField-root': { m: 1, width: '45ch', mb: '50px' } }}
+              noValidate
+              autoComplete="off"
+            >
+              <div>
+                <TextField
+                  id="outlined-select-currency-native"
+                  select
+                  label="Session"
+                  slotProps={{
+                    select: {
+                      native: true,
+                    },
+                  }}
+                  helperText="Please select your session"
+                >
+                  {sessions.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </TextField>
+
+                <TextField
+                  id="outlined-select-currency-native"
+                  select
+                  label="Course"
+                  value={selectedCourse}
+                  onChange={handleCourseChange}
+                  slotProps={{
+                    select: {
+                      native: true,
+                    },
+                  }}
+                  helperText="Please select your course"
+                >
+                  <option value="" disabled></option>
+                  {courses.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </TextField>
+              </div>
+            </Box>
+          </Box>
+
+          <TableContainer component={Paper} sx={{ marginBottom: 4, flexDirection: 'column' }}>
+            <Table sx={{ minWidth: 450 }} size="small" aria-label="a dense table">
+              <TableHead>
+                <TableRow sx={{
+                  backgroundColor: "#E5E7EB", // Tailwind gray-200 hex color
+                }}>
+                  <TableCell sx={{ flex: 0.5 }}>Assesment Type</TableCell>
+                  <TableCell sx={{ flex: 2 }}>Assesment</TableCell>
+                  <TableCell align="left" sx={{ flex: 1 }}>Course (Code) </TableCell>
+                  <TableCell align="left" sx={{ flex: 1 }}>Section Code</TableCell>
+                  <TableCell align="left" sx={{ flex: 1 }}>Teacher</TableCell>
+                  <TableCell align="left" sx={{ flex: 1 }}>Total Marks</TableCell>
+                  <TableCell align="left" sx={{ flex: 1 }}>Marks Obtained</TableCell>
+                </TableRow>
+              </TableHead>
+              {filteredRows.length > 0 ? (
+                filteredRows.map((row, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{row.assementType}</TableCell>
+                    <TableCell>{row.assessment}</TableCell>
+                    <TableCell align="left">{row.courseCode}</TableCell>
+                    <TableCell align="left">{row.sectionCode}</TableCell>
+                    <TableCell align="left">{row.teacher}</TableCell>
+                    <TableCell align="left">{row.totalMarks}</TableCell>
+                    <TableCell align="left">{row.marksObtained}</TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={7} align="center">No data available for the selected course</TableCell>
+                </TableRow>
+              )}
+            </Table>
+          </TableContainer>
+        </Box>
+      </Paper>
+    </Box>
   )
 }
--- src/pages/Exam_pages/SeatingPlan.tsx
+++ src/pages/Exam_pages/SeatingPlan.tsx
@@ -3,66 +3,65 @@ import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, Tab
 
 export const SeatingPlan = () => {
 
-  const data = (examDate:string,startTime:string ,endTime:string, course:string, room:string) => {
-    return{examDate,startTime,endTime,course,room}
+  const data = (examDate: string, startTime: string, endTime: string, course: string, room: string) => {
+    return { examDate, startTime, endTime, course, room }
   }
 
   const rows = [
-    data('05-May-2025','15:30:00','17:00:00','OPERATING SYSTEMS','CG-6'),
-    data('06-May-2025','15:30:00','17:00:00','TECHNICAL WRITING','CF-2'),
-    data('07-May-2025','15:30:00','17:00:00','INTRODUCTION TO DATABASE SYSTEMS,','CF-2'),
-    data('08-May-2025','15:30:00','17:00:00','PROBABLITY & STATISTICS','CF-2'),
-    data('09-May-2025','15:30:00','17:00:00','SOFTWARE DESIGN & ARCHITECTURE.','CG-6'),
-    
+    data('05-May-2025', '15:30:00', '17:00:00', 'OPERATING SYSTEMS', 'CG-6'),
+    data('06-May-2025', '15:30:00', '17:00:00', 'TECHNICAL WRITING', 'CF-2'),
+    data('07-May-2025', '15:30:00', '17:00:00', 'INTRODUCTION TO DATABASE SYSTEMS,', 'CF-2'),
+    data('08-May-2025', '15:30:00', '17:00:00', 'PROBABLITY & STATISTICS', 'CF-2'),
+    data('09-May-2025', '15:30:00', '17:00:00', 'SOFTWARE DESIGN & ARCHITECTURE.', 'CG-6'),
   ]
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', overflow: "auto" }} >
-       <Paper sx={{ padding: 2, overflow: 'auto', maxHeight: '680px' }}>
-              <Box
-                sx={{
-                  position: "sticky",
-                  top: '-17px',
-                  zIndex: 1,
-                  backgroundColor: "white",
-                  textAlign: "center",
-                  py: 2,
-                  minWidth: '1100px',
-                }}
-              >
-              <div className="text-xl font-semibold text-gray-800 sm:text-2xl sm:mb-4 md:text-3xl">Exam Seating Plan</div>
-                
-                <Box sx={{ display: 'flex', flexDirection: 'row', gap: '1rem', marginTop: 4, minWidth: '1100px', width: '100%' }}>
-              {/* First Table */}
-              <TableContainer component={Paper} sx={{ marginBottom: 4, flexDirection: 'column' }}>
-                <Table sx={{ minWidth: 450 }} size="small" aria-label="a dense table">
-                  <TableHead>
-                    <TableRow sx={{
-                      backgroundColor: "#E5E7EB", // Tailwind gray-200 hex color
-                    }}>
-                      <TableCell sx={{ flex: 0.5 }}>Exam Date</TableCell>
-                      <TableCell sx={{ flex: 2 }}>Start Time</TableCell>
-                      <TableCell align="left" sx={{ flex: 1 }}>End Time</TableCell>
-                      <TableCell align="left" sx={{ flex: 1 }}>Course</TableCell>
-                      <TableCell align="left" sx={{ flex: 1 }}>Room</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {rows.map((row,index) => (
-                      <TableRow key={index}>
-                        <TableCell>{row.examDate}</TableCell>
-                        <TableCell>{row.startTime}</TableCell>
-                        <TableCell align="left">{row.endTime}</TableCell>
-                        <TableCell align="left">{row.course}</TableCell>
-                        <TableCell align="left">{row.room}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-              </Box>
+      <Paper sx={{ padding: 2, overflow: 'auto', maxHeight: '680px' }}>
+        <Box
+          sx={{
+            position: "sticky",
+            top: '-17px',
+            zIndex: 1,
+            backgroundColor: "white",
+            textAlign: "center",
+            py: 2,
+            minWidth: '1100px',
+          }}
+        >
+          <div className="text-xl font-semibold text-gray-800 sm:text-2xl sm:mb-4 md:text-3xl">Exam Seating Plan</div>
 
-              </Box>
-          </Paper>
+          <Box sx={{ display: 'flex', flexDirection: 'row', gap: '1rem', marginTop: 4, minWidth: '1100px', width: '100%' }}>
+            {/* First Table */}
+            <TableContainer component={Paper} sx={{ marginBottom: 4, flexDirection: 'column' }}>
+              <Table sx={{ minWidth: 450 }} size="small" aria-label="a dense table">
+                <TableHead>
+                  <TableRow sx={{
+                    backgroundColor: "#E5E7EB", // Tailwind gray-200 hex color
+                  }}>
+                    <TableCell sx={{ flex: 0.5 }}>Exam Date</TableCell>
+                    <TableCell sx={{ flex: 2 }}>Start Time</TableCell>
+                    <TableCell align="left" sx={{ flex: 1 }}>End Time</TableCell>
+                    <TableCell align="left" sx={{ flex: 1 }}>Course</TableCell>
+                    <TableCell align="left" sx={{ flex: 1 }}>Room</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{row.examDate}</TableCell>
+                      <TableCell>{row.startTime}</TableCell>
+                      <TableCell align="left">{row.endTime}</TableCell>
+                      <TableCell align="left">{row.course}</TableCell>
+                      <TableCell align="left">{row.room}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Box>
+
+        </Box>
+      </Paper>
     </Box>
   )
 }
